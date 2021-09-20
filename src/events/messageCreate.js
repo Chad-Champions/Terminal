706a7aa8f3e5bb84d9095267fada929289ef014e
Vignesh D <vigneshd332@gmail.com>
2021-09-21 03:00:36 +0530

    Created Link Filter

---
+++ src/events/messageCreate.js
@@ -0,0 +1,12 @@
+import config from '../../config.js';
+
+export const name = 'messageCreate'
+export const on = true;
+export async function execute(message) {
+    if(message.author.bot) return;
+    
+    if(config.linkFilter && (message.content.includes("http://") || message.content.includes('https://'))) {
+        message.delete();
+        message.channel.send('Please don\'t post links. Not very poggers bro.');        
+    }
+};
