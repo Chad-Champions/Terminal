6544522a12cbc401899e85b7eb92709a5cba432d
Vignesh D <vigneshd332@gmail.com>
2021-09-21 03:01:31 +0530

    Created customized welcome message

--- config.example.js
+++ config.example.js
@@ -1,6 +1,7 @@
 const config = {
-    token: "Your Discord Bot token goes here!",
-    welcomeChannel: "Your Discord Server's Welcome Message Channel ID goes here!"
+    token: "Your Discord Bot token goes here!, WITH the quotes",
+    welcomeChannel: "Your Discord Server's Welcome Message Channel ID goes here!, WITH the quotes",
+    linkFilter: false, // Set to true to enable link filter, WITHOUT quotes
 };
 
 export default config;
--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import { welcomeImage } from 'discord-welcome-card';
 import config from '../../config.js';
 
 export const name = 'guildMemberAdd';
@@ -6,10 +8,34 @@ export async function execute(event, client) {
     const welcomeChannel = await client.channels.cache.get(config.welcomeChannel);
     const user = await client.users.fetch(event.user.id);
 
-    welcomeChannel.send(`Hello ${user}! Welcome to ${event.guild}!`);
     user.createDM().then(message => {
         message.send(`Hello and welcome again to ${event.guild}! Feel free to reply with any questions!`);
     }).catch(err => {
         console.log(err);
-    });   
+    });
+
+    const backgroundImage = fs.readFileSync('./public/assets/background.jpg');
+    const image = await welcomeImage(event, {
+        theme: 'code',
+        text: {
+            color: '#ffffff'
+        },
+        avatar: {
+            outlineWidth: 2,
+            outlineColor: '#eaeaea'
+        },
+        blur: 1,
+        border: true,
+        background: backgroundImage
+    });
+    
+    welcomeChannel.send({
+        content: `Hello ${user}! Welcome to ${event.guild}!`,
+        files: [
+            {
+                attachment: image, 
+                name: 'Welcome.jpg'
+            }
+        ]
+    });
 };
