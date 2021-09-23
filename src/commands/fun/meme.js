162ae152111ae86a510febfa8682ea2d966845e1
Rafi-99 <rafi.md.2018@gmail.com>
2021-09-23 13:57:45 -0500

    Created /meme slash command and added node-fetch dependency

---
+++ src/commands/fun/meme.js
@@ -0,0 +1,40 @@
+import { MessageEmbed } from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import fetch from 'node-fetch';
+
+const meme = {
+    data: new SlashCommandBuilder()
+        .setName('meme')
+        .setDescription('Generates a random Reddit meme')
+        .addStringOption(option => option.setName('subreddit').setDescription('Enter the name of a valid Reddit subreddit').setRequired(true)),
+    async execute(interaction) {
+        const subredditChoice = interaction.options.getString('subreddit');
+
+        fetch(`https://meme-api-node-js.herokuapp.com/api/${subredditChoice}`, {
+            "method": 'GET',
+            "headers": {
+                'Accept': 'application/json'
+            },
+            'mode': 'cors'
+        }).then(response => {
+            response.json().then(result => {
+                if(result.success) {
+                    const memeEmbed = new MessageEmbed()
+                    .setColor('#05055e')
+                    .setTitle(result.data.title)
+                    .setURL(result.data.url)
+                    .setImage(result.data.image)
+                    .setFooter(`${interaction.client.user.username} | © 2022`, interaction.client.user.avatarURL());
+
+                    interaction.reply({ embeds: [memeEmbed] });
+                }
+                else {
+                    return interaction.reply({ content: 'Sorry, we failed to fetch the meme you are looking for. Please try again.' });
+                }
+            });
+        }).catch(err => console.log(err));
+        
+    }
+};
+
+export default meme;
