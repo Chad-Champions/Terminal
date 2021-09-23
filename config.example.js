3f72af704844c8e14949d9429e320471face965e
Rafi-99 <rafi.md.2018@gmail.com>
2021-09-23 15:23:04 -0500

    Removed config.example.js in favor of config.js

--- config.example.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    token: "Your Discord Bot token goes here!, WITH the quotes",
-    clientID : "Your Discord Bot's Client ID, WITH the quotes",
-    welcomeChannel: "Your Discord Server's Welcome Message Channel ID goes here!, WITH the quotes",
-    reactionRolesChannel : "Your Discord Server's ReactionRole Message Channel ID goes here!, WITH the quotes",
-    linkFilter: false, // Set to true to enable link filter, WITHOUT quotes
-};
-
-export default config;
---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    token: process.env.token,
+    guildID: proces.env.guildID,
+    clientID: process.env.clientID,
+    welcomeChannel: process.env.welcomeChannel,
+    reactionRolesChannel: process.env.reactionRolesChannel,
+    linkFilter: process.env.linkFilter
+}
+
+export default config;
