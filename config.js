4cba114aff98ab9585aee5fd6cdf5d882b21e300
Vignesh D <vigneshd332@gmail.com>
2021-09-24 13:46:42 +0530

    Removed Unwanted config var 'guildID'

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const config = {
     token: process.env.token,
-    guildID: process.env.guildID,
     clientID: process.env.clientID,
     welcomeChannel: process.env.welcomeChannel,
     reactionRolesChannel: process.env.reactionRolesChannel,
