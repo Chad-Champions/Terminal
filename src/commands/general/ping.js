e8d46e2047ba0a213965874cac33911c5aac3a9c
Vignesh D <vigneshd332@gmail.com>
2021-09-25 03:08:04 +0530

    Edit Ping to be self deleting

--- src/commands/general/ping.js
+++ src/commands/general/ping.js
@@ -7,6 +7,8 @@ const ping = {
     async execute(interaction) {
         const message = await interaction.reply({ content: 'Pinging...', fetchReply: true });
         interaction.editReply(`Bot Latency: ${message.createdTimestamp - interaction.createdTimestamp}ms \nDiscord API Latency: ${interaction.client.ws.ping}ms`);
+        await new Promise(r => setTimeout(r, 2500));
+        interaction.deleteReply();
     }
 };
 
